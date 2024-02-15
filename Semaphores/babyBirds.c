94b31b1aa39613a4bd62291bfc541e7778313a0a
Jonas Dåderman <Jonasdad@kth.se>
2024-02-15 12:17:23 +0100

    BabyBirds Working

---
+++ Semaphores/babyBirds.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define MAX_BABY_BIRDS 5
+#define MAX_WORMS 20
+int worms = 10;
+sem_t Eatsemaphore;
+sem_t Fillsemaphore;
+//Function Prototypes
+void* consume(void* arg);
+void* produce();
+
+int main(int argc, int* argv){
+
+    pthread_t baby_birds[MAX_BABY_BIRDS];
+    pthread_t mama_bird[1];
+
+    sem_init(&Eatsemaphore, 0, 1);
+    sem_init(&Fillsemaphore, 0, 0);
+
+    for(int i = 0; i < MAX_BABY_BIRDS; i++){
+        pthread_create(&baby_birds[i], NULL, consume, (void*)(i+1));
+    }
+    pthread_create(&mama_bird[1], NULL, produce, NULL); 
+    
+    for(int i = 0; i < MAX_BABY_BIRDS; i++){
+        pthread_join(baby_birds[i], NULL);
+    }
+
+    pthread_join(mama_bird[1], NULL);
+    sem_destroy(&Eatsemaphore);
+    sem_destroy(&Fillsemaphore);
+
+    return 0;
+}
+
+void* consume(void* arg){
+    int bird_ID = (int) arg;
+    while(1){
+        sem_wait(&Eatsemaphore);
+
+        if(worms > 0){
+            int worms_left = --worms;
+            printf("Baby bird #%ld ate a worm, %d left\n", bird_ID, worms_left);
+            sem_post(&Eatsemaphore);
+            sleep(1);
+        }
+        else{
+            printf("Baby bird #%d is chirping\n", bird_ID);
+            sem_post(&Fillsemaphore);
+            sleep(1);
+        }
+
+    }
+}
+void* produce(){
+    while(1){
+        sem_wait(&Fillsemaphore);
+        if(worms == 0){
+            worms = 10;
+            printf("Mama bird gathered %d worms\n", worms);
+            sleep(1);
+        }
+        sem_post(&Eatsemaphore);
+
+    }
+}
