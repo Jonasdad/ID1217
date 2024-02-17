6f927010738da2fdf8ae1eacc25e1f34d700c019
Jonas Dåderman <Jonasdad@kth.se>
2024-02-17 17:13:05 +0100

    Adjusted colors & Fixed "Free" variable bug

--- Monitors/SpaceStation.java
+++ Monitors/SpaceStation.java
@@ -4,7 +4,7 @@ public class SpaceStation implements Runnable {
     Random rand = new Random();
     int free = 6;
     int nitrogen = 10000;
-    int quantum_fluid = 1000;
+    int quantum_fluid = 5000;
     String RESET = "\u001B[0m";
     String BLACK = "\u001B[30m";
     String RED = "\u001B[31m";
@@ -26,21 +26,21 @@ public class SpaceStation implements Runnable {
     }
 
     public void refuel_station() {
-        int fuel_nitrogen = rand.nextInt(4000, 8000);
-        int fuel_quantum_fluid = rand.nextInt(2000, 4000);
+        int fuel_nitrogen = rand.nextInt(2000, 4000);
+        int fuel_quantum_fluid = rand.nextInt(1000, 2000);
         synchronized (this) {
             nitrogen += fuel_nitrogen;
             quantum_fluid += fuel_quantum_fluid;
             print_status();
             notifyAll();
         }
-        System.out.println(GREEN + Thread.currentThread().getName() + " refueled station: " + "+" + fuel_nitrogen
+        System.out.println(BLUE + "Supply "+ Thread.currentThread().getName() + " refueled station: " + "+" + fuel_nitrogen
                 + " nitrogen and " + "+" + fuel_quantum_fluid + " quantum fluid."+RESET);
     }
 
     public void refuel_supply_shuttle(){
-        int nitrogen_requested = rand.nextInt(50, 200);
-        int quantum_fluid_requested = rand.nextInt(10, 100);
+        int nitrogen_requested = rand.nextInt(500, 2000);
+        int quantum_fluid_requested = rand.nextInt(100, 1000);
         synchronized (this) {
             while (nitrogen < nitrogen_requested || quantum_fluid < quantum_fluid_requested) {
                 try {
@@ -54,13 +54,13 @@ public class SpaceStation implements Runnable {
             quantum_fluid -= quantum_fluid_requested;
             print_status();
         }
-        System.out.println(GREEN + "Supply " + Thread.currentThread().getName() + " refueled with " + nitrogen_requested + " nitrogen and "
+        System.out.println(PURPLE + "Supply " + Thread.currentThread().getName() + " refueled with " + nitrogen_requested + " nitrogen and "
                 + quantum_fluid_requested + " quantum fluid."+ RESET);
     }
 
     public void refuel_shuttle() {
-        int nitrogen_requested = rand.nextInt(50, 200);
-        int quantum_fluid_requested = rand.nextInt(10, 100);
+        int nitrogen_requested = rand.nextInt(500, 2000);
+        int quantum_fluid_requested = rand.nextInt(100, 1000);
         synchronized (this) {
             while (nitrogen < nitrogen_requested || quantum_fluid < quantum_fluid_requested) {
                 try {
@@ -79,13 +79,16 @@ public class SpaceStation implements Runnable {
     }
 
     public synchronized void increment_free() throws InterruptedException {
-        if (free < 4) {
+        if (free <= 6) {
             free++;
         }
         notifyAll();
     }
 
     public synchronized void decrement_free() throws InterruptedException {
+        if(free <= 0){
+            System.out.println(RED + "No free slots for supply " + Thread.currentThread().getName() + " waiting." + RESET);
+        }
         while (free <= 0) {
             wait();
         }
@@ -96,7 +99,7 @@ public class SpaceStation implements Runnable {
         try {
             while (true) {
                 int type = rand.nextInt(100);
-                if (type <= 30 && free > 0) {
+                if (type <= 30) {
                     Thread.sleep(rand.nextInt(1500, 5000));
                     decrement_free();
                     System.out.println(CYAN + "Supply "+Thread.currentThread().getName() + " arrived, " + free + " free slots left." + RESET);
@@ -105,13 +108,13 @@ public class SpaceStation implements Runnable {
                     Thread.sleep(rand.nextInt(5000, 6000));
                     increment_free();
                     System.out.println(CYAN + "Supply "+Thread.currentThread().getName() + " departed, " + free + " free slots left." + RESET);
-                } else if (free > 0) {
+                } else {
                     Thread.sleep(rand.nextInt(1500, 5000));
                     decrement_free();
                     System.out.println(YELLOW + "Space "+Thread.currentThread().getName() + " arrived, " + free + " free slots left."+ RESET);
                     refuel_shuttle();
                     increment_free();
-                    System.out.println(YELLOW + "Space " +Thread.currentThread().getName() + " departed, " + free + " free slots left."+ RESET);
+                    System.out.println(GREEN + "Space " +Thread.currentThread().getName() + " departed, " + free + " free slots left."+ RESET);
                     Thread.sleep(rand.nextInt(5000, 6000));
                 }
             }
